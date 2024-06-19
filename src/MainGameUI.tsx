47091279f226625ed5eda9fb9a4836228a723f41
DoubleF3lix <48461905+DoubleF3lix@users.noreply.github.com>
2024-06-18 20:12:09 -0500

    Got player UI looking just right

--- src/MainGameUI.tsx
+++ src/MainGameUI.tsx
@@ -1,14 +1,12 @@
 import { useContext, useEffect } from "react";
 import ChatBox from "./ChatBox";
 import WebSocketConnection from "./WebSocketConnection";
-import { GetGameInfoPacket, GetGameInfoResponsePacket } from "./Packet";
-import { populateFromPacket } from "./GameData";
+import { GetGameInfoPacket, GetGameInfoResponsePacket, PlayerJoinedPacket } from "./Packet";
+import { addPlayer, populateFromPacket } from "./GameData";
 import { useDispatch, useSelector } from "react-redux";
 import { IRootState } from "./Store";
 import { ServerConnectionContext } from "./ServerConnectionContext";
 import Player from "./Player";
-import { Player as PlayerObj } from "./GameData";
-
 
 
 export default function MainGameUI() {
@@ -27,22 +25,31 @@ export default function MainGameUI() {
 
             // TODO if turnCount == -1 and gameData.currentHost = gameData.clientUsername, show start game button
         }));
-    }, []);
 
-    const chunkedPlayers: PlayerObj[][] = [];
-    for (let i = 0; i < players.length; i += 2) {
-        chunkedPlayers.push(players.slice(i, i + 2));
-    }
+        const playerJoinedSubscription = serverConnection.subscribeToServerPacket("playerJoined", (packet) => {
+            packet = packet as PlayerJoinedPacket;
+            if (!players.some((player) => player.username === packet.player.username)) {
+                dispatch(addPlayer(packet.player));
+            }
+        });
+
+        return () => {
+            serverConnection.unsubscribeFromServerPacket(playerJoinedSubscription);
+        };
+    }, []);
 
     return (
         <div className="flex flex-col h-dvh">
-            <p className="text-center font-bold text-base lg:text-lg pt-3">EPIC GAME - CODE - Player 1's Turn</p>
+            <div className="flex flex-row justify-center m-1">
+                <p className="text-center font-bold text-base lg:text-lg pt-3">EPIC GAME - CODE - Player 1's Turn</p>
+                <button className="bg-gray-600 px-2 mx-0.5 text-white rounded h-8 self-end">Kick Player</button>
+                <button className="bg-gray-600 px-2 mx-0.5 text-white rounded h-8 self-end">Start Game</button>
+            </div>
             <hr></hr>
             <div className="flex flex-col lg:flex-row overflow-auto"> 
                 <div className="w-full overflow-auto grid auto-rows-min sm:grid-cols-2 xl:grid-cols-3">
-                    {chunkedPlayers.map((playersChunk) => <>
-                        <Player key={playersChunk[0].username} player={playersChunk[0]}/>
-                        {playersChunk.length == 2 ? <Player key={playersChunk[1].username} player={playersChunk[1]}/> : <></>}
+                    {players.map((player) => <>
+                        <Player key={player.username} player={player}/>
                     </>)}
                 </div>
 
