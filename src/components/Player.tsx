251cb01b3f771f1ae9d89641f0f3caac0e4fbb19
DoubleF3lix <48461905+DoubleF3lix@users.noreply.github.com>
2024-07-27 22:39:11 -0500

    Items now show "No Items" if you don't have any, disabled player picking if the game has not started

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,3 +1,7 @@
+import { useSelector } from "react-redux";
+
+import { selectCurrentPlayer, selectHost } from "~/store/Selectors";
+
 import { PlayerType } from "~/types/PlayerType";
 
 
@@ -7,24 +11,30 @@ type PlayerArgs = {
 
 
 export default function Player({ player }: PlayerArgs) {
+    const currentPlayer = useSelector(selectCurrentPlayer);
+    const currentHost = useSelector(selectHost);
+
     return (
         <div className="flex flex-col flex-grow border-solid border-black border-2 rounded-lg p-3 m-3 lg:p-4 lg:m-4">
             <p>
                 <strong>{player.username}</strong> - {player.lives} {player.lives !== 1 ? "lives" : "life"}
             </p>
+
             <br/>
             <br/>
-            {/* TODO only show when game started */}
-            <p>Items:</p>
-            <ul className="list-disc list-inside">
-                {/* TODO need collapsible items (like Double Damage x2) */}
-                {player.items.map((item, index) => <li key={index + "_playerItem"}>{item}</li>)} 
-            </ul>
+
+            {currentPlayer?.items.length !== 0 ? (<>
+                <p>Items:</p>
+                <ul className="list-disc list-inside">
+                    {/* TODO need collapsible items (like Double Damage x2) */}
+                    {player.items.map((item, index) => <li key={index + "_playerItem"}>{item}</li>)} 
+                </ul>
+            </>) : <p>No Items</p>}
+
 
             <div className="flex flex-row mt-auto pt-2">
                 <button className="bg-gray-600 px-2 mx-0.5 text-white rounded h-8 flex-grow disabled:bg-opacity-50">Shoot</button>
-                <button className="bg-gray-600 px-2 mx-0.5 text-white rounded h-8 flex-grow disabled:bg-opacity-50" disabled>Skip</button>
-                <button className="bg-gray-600 px-2 mx-0.5 text-white rounded h-8 flex-grow disabled:bg-opacity-50">Kick</button>
+                {currentPlayer === currentHost ? <button className="bg-gray-600 px-2 mx-0.5 text-white rounded h-8 flex-grow disabled:bg-opacity-50">Kick</button> : <></>}
             </div>
         </div>
     );
