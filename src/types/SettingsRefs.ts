fa02814ba29976f8221b7b91b7c4dca6d5fed9c1
DoubleF3lix <48461905+DoubleF3lix@users.noreply.github.com>
2025-06-14 20:51:04 -0500

    Missed one

--- src/types/SettingsRefs.ts
+++ src/types/SettingsRefs.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { Settings } from "~/types/generated/liveorlive_server";
+import { Settings } from "~/types/generated/liveorlive_server.Models";
 
 export type SettingsRefs = {
     [K in keyof Settings]: React.MutableRefObject<Settings[K]>;
